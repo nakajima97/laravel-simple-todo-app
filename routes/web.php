ff345ef3b145cb6c0243a41222aed95a3e5c9cb1
Yudai Nakajima <kituneudon97@gmail.com>
2022-01-22 12:00:31 +0900

    3 create task list (#4)
    * #3 タスク一覧画面を作成した
    
    * #3 ログイン後の画面をタスク一覧画面に変更
    
    * #3 いろいろ修正
    
    * #3 ファイルの場所が間違っており日本語化がうまくいかなかったので修正

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,8 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
+Route::get('/task', function () {
+    return view('task_list');
+})->middleware(['auth'])->name('task');
+
 require __DIR__.'/auth.php';
