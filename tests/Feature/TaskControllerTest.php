ca3e17eca5ee16d90904f26adb9a81c8ffe3e0eb
Yudai Nakajima <kituneudon97@gmail.com>
2022-01-28 08:59:57 +0900

    #12 ログインしていないとタスク画面にアクセスできないことを検証するコードを作成した

---
+++ tests/Feature/TaskControllerTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\User;
+
+class TaskControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_cant_access_task_list()
+    {
+        $response = $this->get('/task');
+
+        $response->assertStatus(302);
+    }
+
+    public function test_can_access_task_list()
+    {
+        $user = User::factory()->create();
+
+        $response = $this->actingAs($user)
+                         ->withSession(['banned' => false])
+                         ->get('/task');
+
+        $response->assertStatus(200);
+    }
+}
