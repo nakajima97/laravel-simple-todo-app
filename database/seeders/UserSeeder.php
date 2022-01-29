68b5ce4c595f8911c49ddd67630d0097d2b487e2
Yudai Nakajima <kituneudon97@gmail.com>
2022-01-29 18:21:39 +0900

    #20 seederを動くようにした

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class UserSeeder extends Seeder
@@ -13,6 +14,8 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        //
+        return User::factory()
+                ->count(2)
+                ->create();
     }
 }
