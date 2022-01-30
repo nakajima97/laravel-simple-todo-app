5ed3b972f176190b1279d4e306ba8b74a166e7d4
Yudai Nakajima <kituneudon97@gmail.com>
2022-01-31 08:47:53 +0900

    #19 デバッグ用のコードを削除

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -36,7 +36,7 @@ class TaskController extends Controller
         $task->is_deleted = true;
         $task-> save();
 
-        return ['result' => true, 'message' => 'Task is deleted !', 'task' => $task, 'id' => $id];
+        return ['result' => true, 'message' => 'Task is deleted !'];
     }
 
     public function finish(Request $request)
